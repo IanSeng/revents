1b5576b192c5c871b49ac4c9492bfc8bc6c16749
Ian Seng <ian@Ians-MacBook-Pro.local>
2019-05-30 18:17:52 +0800

    Add event list and form

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -1,21 +1,26 @@
-import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import EventList from "../EventList/EventList";
+import EventForm from "../EventForm/EventForm"
 
 class EventDashboard extends Component {
-    render() {
-        return (
-            <div>
-                <Grid>
-                  <Grid.Column width={10}>
-                    <h2>Left Column</h2>
-                  </Grid.Column>
-                  <Grid.Column width={6}>
-                    <h2>Right Column</h2>
-                  </Grid.Column>
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Grid.Column width={10}>
+            <h2>Event List</h2>
+            <EventList />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Button positive content='Create Event'/>
+            <EventForm/>
+            <h2>Right Column</h2>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default  EventDashboard
+export default EventDashboard;
