e4a4e81c4c9634151e7abffabd317e6512fb7388
Ian Seng <ian@Ians-MacBook-Pro-2.local>
2019-08-15 01:47:35 +0800

    Add Google login to the app

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -7,7 +7,7 @@ import thunk from "redux-thunk";
 import firebase from "../../config/firebase";
 
 const rrfConfig = {
-  userProfile: "user",
+  userProfile: "users",
   attachAuthIsReady: true,
   useFirestoreForProfile: true
 };
--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -52,10 +52,11 @@ export const socialLogin = (selectedProvider) =>
    const firebase = getFirebase();
    try {
      dispatch(closeModal());
-     await firebase.login({
+     const user = await firebase.login({
        provider: selectedProvider,
        type: 'popup'
      })
+     console.log(user);
    } catch(error) {
      console.log(error)
    }
